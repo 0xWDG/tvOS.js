c41f17893fb390bd84f66ac35ee5f3cf11a79fd2
Wesley de Groot <wes@vista.aero>
2015-11-06 19:59:51 +0100

    tvOS.js

--- toGit.sh
+++ toGit.sh
@@ -1,21 +1,21 @@
 # Push wiki
 cd tvOS.wiki
-git pull &>/dev/null
-git add . &>/dev/null
-git commit -m "tvOS.js" &>/dev/null
-git push &>/dev/null
+git pull
+git add .
+git commit -m "tvOS.js"
+git push
 cd ..
 
 # Push wiki data
 cd tvOS.wiki-data
-git pull &>/dev/null
-git add . &>/dev/null
-git commit -m "tvOS.js" &>/dev/null
-git push &>/dev/null
+git pull
+git add .
+git commit -m "tvOS.js"
+git push
 cd ..
 
 # Push main
-git pull &>/dev/null
-git add . &>/dev/null
-git commit -m "tvOS.js" &>/dev/null
-git push &>/dev/null
+git pull
+git add .
+git commit -m "tvOS.js"
+git push
